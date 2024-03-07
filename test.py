d8cc369b3388d6db88d633aa41904927bd93da62
Anuj Patel <anuj-patel@outlook.com>
2024-03-07 13:28:49 -0600

    Delete test file

--- test.py
+++
@@ -1,6 +0,0 @@
-test_client = app.test_client()
-async with test_client.websocket('/ws/') as test_websocket:
-    await test_websocket.send(data)
-    result = await test_websocket.receive()
-
-    
